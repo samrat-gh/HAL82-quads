d91b421087a797ef4b4ceee953fc87156f2b68a0
Samrat Ghimire <samratghimire32@gmail.com>
2026-02-20 12:15:00 +0545

    boilerplate

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      // new URL("https://ferf1mheo22r9ira.public.blob.vercel-storage.com/**"),
+    ],
+  },
+};
+
+export default nextConfig;
